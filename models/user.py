81a9bfabd5c5568c5669eecf0752ae7fbdb41f42
Toluwaloju0 <teecoded1999@gmail.com>
2024-09-24 07:46:52 +0100

    project update

--- models/user.py
+++ models/user.py
@@ -6,11 +6,13 @@ from models.many import user_skill
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
+
 class User(BaseModel, Base):
     """The user class"""
     __tablename__ = 'users'
 
     name = Column(String(100), nullable=False)
+    password = Column(String(1024), nullable=False)
     skill_id = Column(String(60), ForeignKey('skills.id'))
     badge_id = Column(String(60), ForeignKey('badges.id'))
 
